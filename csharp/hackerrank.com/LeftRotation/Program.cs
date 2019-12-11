afb75f32af0d21e965f336aeb50cea27018200cd
Nuno Cancelo <nuno.cancelo@gmail.com>
2019-12-11 16:24:45 +0000

    Arrays: Left Rotation

---
+++ csharp/hackerrank.com/LeftRotation/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d)
+    {
+        int idxToCopy = a.Length - d;
+        int[] result = new int[a.Length];
+        int idx = 0;
+        // starting on index (d) on array a, copy to index 0 of array result.
+        for(;idx<idxToCopy;idx++)
+        {
+            result[idx] = a[idx + d];
+        }
+        // starting on index (0) to (d) copy to index n-d 
+        for(int j = 0;j < d; j++, idx++ )
+        {
+            result[idx] = a[j];
+        }
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+       // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nd = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nd[0]);
+
+        int d = Convert.ToInt32(nd[1]);
+
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+        ;
+        int[] result = rotLeft(a, d);
+
+        Console.WriteLine(string.Join(" ", result));
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
